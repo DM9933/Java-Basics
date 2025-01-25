fc8853d5b1110790099f7d4e69a676a719c4772f
Dabarjun Mazumdar <113283253+DM9933@users.noreply.github.com>
2025-01-25 12:30:30 +0600

    Add files via upload

---
+++ Java_Basics/src/main/java/Excercise/DivisibilityCheckNumber.java
@@ -0,0 +1,23 @@
+package Excercise;
+
+import java.util.Scanner;
+
+public class DivisibilityCheckNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Get user-defined range
+        System.out.print("Enter the start of the range: ");
+        int start = scanner.nextInt();
+        System.out.print("Enter the end of the range: ");
+        int end = scanner.nextInt();
+
+        System.out.println("Numbers not divisible by 2 or 3 in the range:");
+        for (int i = start; i <= end; i++) {
+            if (i % 2 != 0 && i % 3 != 0) {
+                System.out.print(i + " ");
+            }
+        }
+        scanner.close();
+    }
+}
